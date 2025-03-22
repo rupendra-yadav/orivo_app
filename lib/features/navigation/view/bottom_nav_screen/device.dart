57dcdc701e2782e2a097f56a9a35549bbaad2cde
shantanusarkar1001 <shantanusarkar1001@gmail.com>
2025-03-22 18:25:36 +0530

    timer added to work on background and Call the Device List API after 15 min.

--- lib/features/navigation/view/bottom_nav_screen/device.dart
+++ lib/features/navigation/view/bottom_nav_screen/device.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:auro/features/navigation/view/bottom_nav_screen/controller/device_list_controller.dart';
 import 'package:auro/features/navigation/view/bottom_nav_screen/widgets/device_list.dart';
 import 'package:auro/utils/constant/colors.dart';
@@ -7,29 +9,53 @@ import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../../../../utils/constant/text_strings.dart';
 import '../../../../utils/preferences/cache_manager.dart';
 
-class Device extends StatelessWidget {
+class Device extends StatefulWidget {
   const Device({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // Initialize the controller using GetX
-    final DeviceListController controller = Get.put(DeviceListController());
+  State<Device> createState() => _DeviceState();
+}
 
-    // Fetch the device list when the widget is built
-    // controller.getDeviceList();
-    // controller.getDeviceList2();
-    controller.loadDeviceListFromPrefs();
-   // controller.updateFcm();
+class _DeviceState extends State<Device> {
+  // Initialize the controller using GetX
+  final DeviceListController controller = Get.put(DeviceListController());
 
+  // Fetch the device list when the widget is built
+  // controller.getDeviceList();
+  // controller.getDeviceList2();
+  // controller.updateFcm();
 
+  String _timerStatus = "Timer Not Started";
+  bool _timerRunning = false;
 
+  void timer() {
+    setState(() {
+      _timerStatus = "Running...";
+      _timerRunning = true;
+    });
 
+    Timer.run(() {
+      Future.delayed(const Duration(minutes: 15), () {
+        // This code will execute after 15 minutes in the background.
 
-    SharedPrefs.getString(TTexts.prefAccessToken);
+        controller.getDeviceList2();
+        // Update the UI on the main thread when the timer completes.
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          if (mounted) {
+            // Check if the widget is still in the tree
+            setState(() {
+              _timerStatus = "Completed!";
+              _timerRunning = false;
+            });
+          }
+        });
+      });
+    });
+  }
 
+  void passwordResetNotification() {
     if (SharedPrefs.getBool("firstLaunch") == true) {
     } else {
       AwesomeNotifications().createNotification(
@@ -39,8 +65,21 @@ class Device extends StatelessWidget {
               title: "Change Password..!",
               body: "For your account’s safety,Update your password.\nClick on the Change Password in Profile."));
     }
+  }
 
-    // Set the login preference
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    controller.loadDeviceListFromPrefs();
+    timer();
+    passwordResetNotification();
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     SharedPrefs.setBool("isLoggedIn", true);
     SharedPrefs.setBool("firstLaunch", true);
 
@@ -59,7 +98,7 @@ class Device extends StatelessWidget {
           RefreshIndicator(
             // The onRefresh callback triggers the controller's getDeviceList method
             onRefresh: () async {
-             // await controller.getDeviceList();
+              // await controller.getDeviceList();
               await controller.getDeviceList2();
             },
             child: SingleChildScrollView(
